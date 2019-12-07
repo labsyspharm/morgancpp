282f306879afc2871dc7a1e4f767eebbfa2cf1a5
Artem Sokolov <artem.sokolov@gmail.com>
2019-12-07 00:03:20 -0500

    Argument verification

--- morgan.cpp
+++ morgan.cpp
@@ -2,8 +2,14 @@
 #include <bitset>
 
 // Converts character hex to character binary
-std::string hex2bin( const std::string& hex )
+std::bitset<2048> hex2bin( const std::string& hex )
 {
+  // Verify that the string contains 2048 bits of data
+  if( hex.length() != 512 )
+    ::Rf_error("Input hex string must be of length 512");
+
+  // Convert hex to binary
+  // Perform the conversion by hand to keep things optimized
   std::string bin;
   for( unsigned int i = 0; i != hex.length(); ++i )
     {
@@ -28,7 +34,9 @@ std::string hex2bin( const std::string& hex )
 	default: bin += "0000"; break;
 	}
     }
-  return bin;
+
+  // Instantiate the bitset from the binary string
+  return std::bitset<2048>(bin);
 }
 
 // Jaccard similarity of two binary strings of length 2048
@@ -41,42 +49,62 @@ double bin_jaccard( const std::bitset<2048>& b1, const std::bitset<2048>& b2 )
 
 // Jaccard similarity of two hexadecimal strings of length 2048/4
 // [[Rcpp::export]]
-double hex_jaccard( const std::string& s1, const std::string& s2 )
+double tanimoto( const std::string& s1, const std::string& s2 )
 {
-  std::bitset<2048> b1( hex2bin(s1) );
-  std::bitset<2048> b2( hex2bin(s2) );
+  std::bitset<2048> b1 = hex2bin(s1);
+  std::bitset<2048> b2 = hex2bin(s2);
   return bin_jaccard( b1, b2 );
 }
 
-// Morgan fingerprint collection
+// Morgan fingerprints collection
 class MorganFPS {
 public:
-  // Constructor
+  // Constructor accepts a character vector of hex strings
   MorganFPS( const std::vector<std::string>& vhx )
   {
     for( unsigned int i = 0; i < vhx.size(); ++i )
       {
-	std::string s = vhx[i];
-	if( s.length() != 512 )
-	  ::Rf_error("Input hex strings must be of length 512");
-
-	// Create a new bit set and add it to collection
-	std::bitset<2048> bs( hex2bin(s) );
+	std::bitset<2048> bs = hex2bin(vhx[i]);
 	vbs.push_back(bs);
       }
   }
 
   // Tanimoto similarity between drugs i and j
-  double tanimoto( unsigned int i, unsigned int j ) {return bin_jaccard( vbs[i-1], vbs[j-1] );}
+  // Adjust for 0-based indexing
+  double tanimoto( unsigned int i, unsigned int j )
+  {
+    if( i >= vbs.size() || j >= vbs.size() )
+      ::Rf_error("Index out of range");
+    return bin_jaccard( vbs[i-1], vbs[j-1] );
+  }
 
   // Tanimoto similarity of drug i to every other drug
   std::vector<double> tanimoto_all( unsigned int i )
   {
+    // Argument verification
+    if( i >= vbs.size() )
+      ::Rf_error("Index out of range");
+
+    // Iterate over the collection
     std::vector<double> res( vbs.size() );
     for( unsigned int ii = 0; ii < vbs.size(); ++ii )
       res[ii] = bin_jaccard( vbs[i-1], vbs[ii] );
     return res;
   }
+
+  // Tanimoto similarity of an external drug to every other drug
+  //   in the collection
+  std::vector<double> tanimoto_ext( const std::string& other )
+  {
+    std::bitset<2048> b = hex2bin(other);
+    std::vector<double> res( vbs.size() );
+    for( unsigned int ii = 0; ii < vbs.size(); ++ii )
+      res[ii] = bin_jaccard( b, vbs[ii] );
+    return res;
+  }
+
+  // Size of the dataset
+  unsigned int size() {return vbs.size() * sizeof(std::bitset<2048>);}
   
 private:
   std::vector< std::bitset<2048> > vbs;
@@ -87,10 +115,15 @@ using namespace Rcpp;
 // Expose all relevant classes through an Rcpp module
 RCPP_EXPOSED_CLASS(MorganFPS)
 RCPP_MODULE(morgan_cpp) {
-  
+
   class_<MorganFPS>( "MorganFPS" )
     .constructor< std::vector<std::string> >()
-    .method("tanimoto", &MorganFPS::tanimoto )
-    .method("tanimoto_all", &MorganFPS::tanimoto_all )
+    .method("size", &MorganFPS::size, "Size of the data in bytes")
+    .method("tanimoto", &MorganFPS::tanimoto,
+	    "Similarity between two fingerprints in the collection")
+    .method("tanimoto_all", &MorganFPS::tanimoto_all,
+	    "Similarity of a fingerprint against all other fingerprints in the collection")
+    .method("tanimoto_ext", &MorganFPS::tanimoto_ext,
+	    "Similarity of an external fingerprints against all fingerprints in the collection")
     ;
 }
